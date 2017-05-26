7494c8c795a6fdaa1cd36ec5089727468f2fb676
Thomas Ferreira <thomas_ferreira@yahoo.fr>
2017-05-27 01:04:06 +0200

    api: add unit tests for accounts and transactions

--- testing_config.py
+++ testing_config.py
@@ -2,14 +2,37 @@ from flask_testing import TestCase
 from application.app import app, db
 from application.models import User
 import os
+from datetime import datetime
 from setup import basedir
 import json
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+
+        return json.JSONEncoder.default(self, o)
 
 
 class BaseTestConfig(TestCase):
     default_user = {
         "email": "default@gmail.com",
         "password": "something2"
+    }
+
+    default_account = {
+        "label": "test account",
+        "bank": "some bank",
+        "iban": "FR76 0000 0000 0000 0000",
+        "bic": "TESTFRZZ"
+    }
+
+    default_transaction = {
+        "account_id": 1,
+        "label": "some_transaction",
+        "amount": 10.33,
+        "date": datetime.now()
     }
 
     def create_app(self):
@@ -19,13 +42,34 @@ class BaseTestConfig(TestCase):
     def setUp(self):
         self.app = self.create_app().test_client()
         db.create_all()
+
+        # create user
         res = self.app.post(
-                "/api/create_user",
-                data=json.dumps(self.default_user),
-                content_type='application/json'
+            "/api/create_user",
+            data=json.dumps(self.default_user),
+            content_type='application/json'
+        )
+        self.token = json.loads(res.data.decode("utf-8"))["token"]
+
+        with self.app.session_transaction() as sess:
+            sess['user_id'] = 1
+            sess['_fresh'] = True  # https://flask-login.readthedocs.org/en/latest/#fresh-logins
+
+        # create account
+        self.app.post(
+            "/api/accounts/create",
+            data=json.dumps(self.default_account),
+            content_type='application/json',
+            headers={'Authorization': self.token}
         )
 
-        self.token = json.loads(res.data.decode("utf-8"))["token"]
+        # create transaction
+        self.app.post(
+            "/api/transactions/create",
+            data=self.default_transaction,
+            content_type='application/json',
+            headers={'Authorization': self.token}
+        )
 
     def tearDown(self):
         db.session.remove()
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,11 +1,18 @@
 from testing_config import BaseTestConfig
-from application.models import User
+from application.models import User, Account
 
 
 class TestModels(BaseTestConfig):
     def test_get_user_with_email_and_password(self):
         self.assertTrue(
-                User.get_user_with_email_and_password(
-                        self.default_user["email"],
-                        self.default_user["password"])
+            User.get_user_with_email_and_password(
+                self.default_user["email"],
+                self.default_user["password"]
+            )
+        )
+
+    def test_get_accounts_by_user(self):
+        user = {'id': 1}
+        self.assertTrue(
+            len(Account.get_accounts(user))
         )

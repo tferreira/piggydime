0033abf86dce7920a736d7cb8d277e1d661315f1
Thomas Ferreira <thomas_ferreira@yahoo.fr>
2017-08-03 00:52:08 +0200

    Add translations for home with stepper

--- static/src/components/Home/index.js
+++ static/src/components/Home/index.js
@@ -28,11 +28,11 @@ class Home extends React.Component {
   getStepContent(stepIndex) {
     switch (stepIndex) {
       case 0:
-        return <FormattedMessage id="home.steps.one" />
+        return <FormattedMessage id="home.steps.one.description" />
       case 1:
-        return <FormattedMessage id="home.steps.two" />
+        return <FormattedMessage id="home.steps.two.description" />
       case 2:
-        return <FormattedMessage id="home.steps.three" />
+        return <FormattedMessage id="home.steps.three.description" />
       default:
         return 'I want to break free!'
     }
@@ -43,16 +43,24 @@ class Home extends React.Component {
     const contentStyle = { margin: '0 16px' }
     return (
       <div className="container text-center">
-        <h1>Hello, welcome on Piggydime.</h1>
+        <h1>
+          <FormattedMessage id="home.welcome" />
+        </h1>
         <Stepper activeStep={stepIndex}>
           <Step>
-            <StepLabel>Login or register</StepLabel>
+            <StepLabel>
+              <FormattedMessage id="home.steps.one.title" />
+            </StepLabel>
           </Step>
           <Step>
-            <StepLabel>Create an account</StepLabel>
+            <StepLabel>
+              <FormattedMessage id="home.steps.two.title" />
+            </StepLabel>
           </Step>
           <Step>
-            <StepLabel>Recurring transactions</StepLabel>
+            <StepLabel>
+              <FormattedMessage id="home.steps.three.title" />
+            </StepLabel>
           </Step>
         </Stepper>
         <div style={contentStyle}>
@@ -65,9 +73,9 @@ class Home extends React.Component {
                     this.setState({ stepIndex: 0, finished: false })
                   }}
                 >
-                  Click here
+                  <FormattedMessage id="stepper.clickHere" />
                 </a>{' '}
-                to go back to the beginning.
+                <FormattedMessage id="stepper.backToBeginning" />
               </p>
             : <div>
                 <p>
@@ -75,13 +83,17 @@ class Home extends React.Component {
                 </p>
                 <div style={{ marginTop: 12 }}>
                   <FlatButton
-                    label="Back"
+                    label={<FormattedMessage id="stepper.back" />}
                     disabled={stepIndex === 0}
                     onTouchTap={this.handlePrev}
                     style={{ marginRight: 12 }}
                   />
                   <RaisedButton
-                    label={stepIndex === 2 ? 'Finish' : 'Next'}
+                    label={
+                      stepIndex === 2
+                        ? <FormattedMessage id="stepper.finish" />
+                        : <FormattedMessage id="stepper.next" />
+                    }
                     primary={true}
                     onTouchTap={this.handleNext}
                   />
--- static/src/i18n/messages.js
+++ static/src/i18n/messages.js
@@ -1,24 +1,58 @@
 export default {
   en: {
+    stepper: {
+      next: 'Next',
+      back: 'Back',
+      finish: 'Finish',
+      clickHere: 'Click here',
+      backToBeginning: 'to go back to the beginning.'
+    },
     home: {
       steps: {
-        one: 'Use the left menu to log in or register an account.',
-        two:
-          'Once logged in, you will be able to create your first bank account using the "plus sign" round button.',
-        three:
-          'There is a feature to schedule monthly or yearly transctions. Then edit your account projected date parameter to know your future account balance.'
-      }
+        one: {
+          title: 'Login or register',
+          description: 'Use the left menu to log in or register an account.'
+        },
+        two: {
+          title: 'Create an account',
+          description:
+            'Once logged in, you will be able to create your first bank account using the "plus sign" round button.'
+        },
+        three: {
+          title: 'Recurring transactions',
+          description:
+            'There is a feature to schedule monthly or yearly transctions. Then edit your account projected date parameter to know your future account balance.'
+        }
+      },
+      welcome: 'Hello, welcome on Piggydime.'
     }
   },
   fr: {
+    stepper: {
+      next: 'Suivant',
+      back: 'Précédent',
+      finish: 'Terminer',
+      clickHere: 'Cliquez ici',
+      backToBeginning: 'pour revenir au début.'
+    },
     home: {
       steps: {
-        one: 'Dépliez le menu de gauche pour vous identifier.',
-        two:
-          'Une fois identifié, créez votre premier compte bancaire en utilisant le bouton "plus".',
-        three:
-          'Planifiez des transactions mensuelles ou annuelles. Editez les options de votre compte et définissez la date de solde prévisionnel.'
-      }
+        one: {
+          title: 'Identifiez-vous',
+          description: 'Dépliez le menu de gauche pour vous identifier.'
+        },
+        two: {
+          title: 'Créez un compte bancaire',
+          description:
+            'Une fois identifié, créez votre premier compte bancaire en utilisant le bouton "plus".'
+        },
+        three: {
+          title: 'Transactions récurrentes',
+          description:
+            'Planifiez des transactions mensuelles ou annuelles. Editez les options de votre compte et définissez la date de solde prévisionnel.'
+        }
+      },
+      welcome: 'Bienvenue sur Piggydime !'
     }
   }
 }
